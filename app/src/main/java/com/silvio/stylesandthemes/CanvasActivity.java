3147fe8478a758d9bb1d814d0ef507419fe320b0
Silvio Antonio <oliveira_0000@hotmail.com>
2019-11-09 15:30:58 -0300

    Metodo construtor e setter radius

--- app/src/main/java/com/silvio/stylesandthemes/CanvasActivity.java
+++ app/src/main/java/com/silvio/stylesandthemes/CanvasActivity.java
@@ -1,14 +1,46 @@
 package com.silvio.stylesandthemes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
 
-public class CanvasActivity extends AppCompatActivity {
+import androidx.annotation.Nullable;
+import androidx.interpolator.view.animation.LinearOutSlowInInterpolator;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_canvas);
+public class CanvasActivity extends View {
+
+    private float radius;
+    private float x;
+    private float y;
+    private final Paint mPaint = new Paint();
+    private AnimatorSet pulseAnimatorSet = new AnimatorSet();
+    private static final int COLOR_ADJUSTER = 5;
+    private static final int ANIMATION_DURATION = 4000;
+    private static final int ANIMATION_DELAY = 1000;
+
+    public CanvasActivity(Context context) {
+        this(context, null);
     }
+
+    public CanvasActivity(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+        this.mPaint.setColor(Color.GREEN + (int) radius / COLOR_ADJUSTER);
+        invalidate();
+    }
+
+
+
 }
--- app/src/main/java/com/silvio/stylesandthemes/MainActivity.java
+++ app/src/main/java/com/silvio/stylesandthemes/MainActivity.java
@@ -9,17 +9,15 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.btn);
+        Button button = findViewById(R.id.btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MainActivity.class));
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
             }
         });
     }
