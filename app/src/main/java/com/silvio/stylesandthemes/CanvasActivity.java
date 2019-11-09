7c1fe91fc26f8738ae1fa96a8a0062c8b01849c1
Silvio Antonio <oliveira_0000@hotmail.com>
2019-11-09 10:20:20 -0300

    Criado classe para desenho de canvas

---
+++ app/src/main/java/com/silvio/stylesandthemes/CanvasActivity.java
@@ -0,0 +1,14 @@
+package com.silvio.stylesandthemes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CanvasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canvas);
+    }
+}
--- app/src/main/java/com/silvio/stylesandthemes/MainActivity.java
+++ app/src/main/java/com/silvio/stylesandthemes/MainActivity.java
@@ -2,14 +2,25 @@ package com.silvio.stylesandthemes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        });
     }
 }
